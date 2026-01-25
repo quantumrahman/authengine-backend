ef5d5b5f7c02ebc00dc53f19998067e0a894f2c1
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-25 21:29:33 +0600

    implement basic sign-up controller structure in authControllers.js file

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,7 @@
+// import modules -------------------------------------->
+import 'dotenv/config';
+
+// sign-up controller ---------------------------------->
+export const signUpController = async (req, res) => {
+    console.log('hello from sign-up controller.');
+};
